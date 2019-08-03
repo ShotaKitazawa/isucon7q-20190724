b4e96fe94777733c338fef4d64a79cb740fbfceb
ShotaKitazawa <skitazawa1121@gmail.com>
2019-08-03 19:51:51 +0900

    update

--- src/isubata/app.go
+++ src/isubata/app.go
@@ -48,27 +48,12 @@ func init() {
 	crand.Read(seedBuf)
 	rand.Seed(int64(binary.LittleEndian.Uint64(seedBuf)))
 
-	db_host := os.Getenv("ISUBATA_DB_HOST")
-	if db_host == "" {
-		//db_host = "127.0.0.1"
-		db_host = "10.128.0.2"
-	}
-	db_port := os.Getenv("ISUBATA_DB_PORT")
-	if db_port == "" {
-		db_port = "3306"
-	}
-	db_user := os.Getenv("ISUBATA_DB_USER")
-	if db_user == "" {
-		//db_user = "root"
-		db_user = "isucon"
-	}
-	db_password := os.Getenv("ISUBATA_DB_PASSWORD")
-	if db_password != "" {
-		//db_password = ":" + db_password
-		db_password = "isucon"
-	}
+	db_host = "10.128.0.2"
+	db_port = "3306"
+	db_user = "isucon"
+	db_password = "isucon"
 
-	dsn := fmt.Sprintf("%s%s@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4",
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/isubata?parseTime=true&loc=Local&charset=utf8mb4",
 		db_user, db_password, db_host, db_port)
 
 	log.Printf("Connecting to db: %q", dsn)
